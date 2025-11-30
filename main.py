ee656c86574eb8d66c8376bfcc340f919fbf10c8
Yan Augusto <augustoyan446@gmail.com>
2025-11-29 21:38:34 -0300

    nv

--- main.py
+++ main.py
@@ -3,7 +3,19 @@ print("Quero meu primeiro emprego!")
 nome = "Programador Python"
 idade = 27
 peso = 70.3
+conta = 2 
+contas = 3
 
+is_python = true
+is_java = false
+
+ingressos = 50
+compradores = 250
+tem_imgresso_suficiente = (ingressos <= compradores)
+print(tem_ingresso_suficiente)
+
+print(conta)
 print(nome)
 print(idade)
 print(peso) 
+print(conta + contas)
