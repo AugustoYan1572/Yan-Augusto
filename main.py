5985533ff5fa2c1c763cca6efec9ffccec83d963
Yan Augusto <augustoyan446@gmail.com>
2025-11-30 18:31:26 -0300

    nv py

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ peso = 70.3
 conta = 2 
 contas = 3
 
-is_python = true
-is_java = false
-
 ingressos = 50
 compradores = 250
-tem_imgresso_suficiente = (ingressos <= compradores)
+tem_ingresso_suficiente = (ingressos >= compradores)
 print(tem_ingresso_suficiente)
 
 print(conta)
